eba3bd15a87f5e8491eff4e995d423f5a5ef1ba9
Niklas Portmann <niklaspor@gmail.com>
2022-04-01 19:08:40 +0200

    implement example page

---
+++ website/src/App.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+  import prettier from "prettier/standalone";
+  import pluginHtml from "prettier/parser-html";
+  import pluginBabel from "prettier/parser-babel";
+  import pluginCSS from "prettier/parser-postcss";
+  import pluginGoTemplate from "../../lib/index";
+
+  let input = "";
+  let output = "";
+
+  function onInput(event) {
+    try {
+      output = prettier.format(event.target.value, {
+        parser: "go-template",
+        plugins: [pluginHtml, pluginBabel, pluginCSS, pluginGoTemplate],
+      });
+    } catch (e) {
+      output = e.toString();
+    }
+  }
+</script>
+
+<main>
+  <textarea bind:value={input} on:input={onInput} />
+  <div class="divider" />
+  <textarea bind:value={output} class="output-only" disabled />
+</main>
+
+<style>
+  .output-only {
+    pointer-events: none;
+  }
+  .divider {
+    width: 1px;
+    background-color: darkgray;
+  }
+  textarea {
+    outline: none;
+    border-radius: 0;
+    flex-grow: 1;
+    font-family: monospace;
+    white-space: pre-wrap;
+  }
+  main {
+    display: flex;
+    justify-items: stretch;
+    height: 100%;
+    width: 100%;
+  }
+</style>
